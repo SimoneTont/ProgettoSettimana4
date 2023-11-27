f5fe79cc7d256dce622723cc17084c9a3a465ef1
Simone Tonti <simone.tonti98@gmail.com>
2023-11-27 22:25:30 +0100

    Responsive

--- S5-P-Medium-Clone-Start/assets/js/script.js
+++ S5-P-Medium-Clone-Start/assets/js/script.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  if (document.documentElement.scrollTop > 400) {
+  if (document.documentElement.scrollTop > (window.innerHeight/2)) {
     document.querySelector("button").className= "SfondoVerde";
     document.querySelector("header").className= "SfondoBianco";
   } else {
